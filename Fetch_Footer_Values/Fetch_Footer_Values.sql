ed76f0ffec90a5e6c963944f59a9463662379dc5
Rohit Sonawane <rds22120@gmail.com>
2024-12-21 17:16:03 +0530

    Add problem statement comment to SQL script for fetching footer values

--- Fetch_Footer_Values/Fetch_Footer_Values.sql
+++ Fetch_Footer_Values/Fetch_Footer_Values.sql
@@ -1,4 +1,4 @@
-
+-- PROBLEM STATEMENT: 
 -- Write a sql query to return the footer values from input table, meaning all the last 
 -- non null values from each field as shown in expected output.									
 
